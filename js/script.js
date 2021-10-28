4c0d6c329c418188a35ee098ebb3bb9aab0275ba
Caleb Faulks <caleb.faulks@gmail.com>
2021-10-28 16:01:14 +1100

    Loading Screen

--- js/script.js
+++ js/script.js
@@ -6,14 +6,22 @@ var pageNames = [
 	"contact"
 ];
 
+window.addEventListener('load', window_load);
 window.addEventListener('hashchange', checkNavChange);
 
 function loadPost() {
 	if (location.hash) checkNavChange();
+}
+function window_load() {
+	setTimeout(hideLoadOverlay, 250);
 }
 
 function checkNavChange(e) {
 	if (!location.hash) return;
 	var hash = location.hash.slice(1).toLowerCase();
 	if (pageNames.includes(hash)) document.body.className = hash + "-active";
+}
+
+function hideLoadOverlay() {
+	document.querySelector(".loading-overlay").classList.add("not-loading");
 }
